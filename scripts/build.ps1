8e3d5c785c6a007bb34397647a65a7f31b30fdc3
Seasawher <tellagoodstory@icloud.com>
2024-08-18 21:42:29 +0900

    fix workflow

---
+++ scripts/build.ps1
@@ -0,0 +1,4 @@
+lake env lean --run Lean/Tactic.lean > src/tactics.md
+lake env lean --run Lean/Option.lean > src/options.md
+lake env lean --run Lean/Command.lean > src/commands.md
+lake env lean --run Lean/Attribute.lean > src/attributes.md
