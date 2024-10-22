0812b55e8de44182d32566857b09ee9fdba815cd
Seasawher <tellagoodstory@icloud.com>
2024-10-22 09:34:29 +0900

    update `build.ps1` and fix bug

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,3 @@
-Get-ChildItem -Path docs -Exclude SUMMARY.md | Remove-Item -Force -Recurse
-
 lake env lean --run Lean/Tactic.lean > docs/tactics.md
 lake env lean --run Lean/Option.lean > docs/options.md
 lake env lean --run Lean/Command.lean > docs/commands.md
